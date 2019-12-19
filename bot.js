c759e58e5dbaab116202ca5a4797566c1024f720
adri51 <adriibm91@gmail.com>
2019-12-19 17:34:49 +0100

    Final de bot

--- bot.js
+++ bot.js
@@ -10,14 +10,7 @@ bot.on('polling_error', function (error) {
     console.log(error);
 });
 
-bot.onText(/^\/start/, function (msg) {
-    var chatId = msg.chat.id;
-    var nameUser = msg.from.first_name;
-
-    bot.sendMessage(chatId, "Bienvenido a mi bot" + nameUser);
-});
-
-bot.onText(/^\/oferta/, (msg) => {
+bot.onText(/^\Oferta/, (msg) => {
     axios.get('https://xz94zfs6u8.execute-api.eu-west-1.amazonaws.com/default/myBakery')
         .then(response => {
             bot.sendMessage(msg.chat.id, "La oferta del dia es " + response.data);
@@ -25,6 +18,10 @@ bot.onText(/^\/oferta/, (msg) => {
         })
 });
 
-bot.onText(/^\Hola/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Hola " + msg.from.first_name);
+bot.onText(/^\oferta/, (msg) => {
+    axios.get('https://xz94zfs6u8.execute-api.eu-west-1.amazonaws.com/default/myBakery')
+        .then(response => {
+            bot.sendMessage(msg.chat.id, "La oferta del dia es " + response.data);
+
+        })
 });
